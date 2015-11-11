6ad06a768e2ff1d9f1ef4bd75a50da09c260d278
Luis Pastrana <lpastrana2@hotmail.com>
2015-11-10 20:14:12 -0500

    Update for index.html, main.css and app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
 $(document).ready(function() { 
-// Creates list items from the input text field when the 'add' button is clicked  
- $("#add").click(function() {
-      var item= $('#items')
+  // Creates list items from the input text field when the 'add' button is clicked  
+  $("#add").click(function() {
+      var item= $('#items');
      $('.cat').prepend('<li><input type="checkbox" id="check">'+item.val()+'<button id="remove">X</button></li>');
     });
 // On click removes the list items individually  
     $('.cat').on('click', '#remove', function() {
       $(this).parent('li').remove();
-    });  
+    });
 // Once the input field is selected it will remove any text present
     $('input').on('click focusin', function() {
       this.value = '';
     }); 
-
-   // $('checkbox').on('click', function() {
-    
- //     $('#items li').addClass('line');
-    //  alert('algo');    
-  //  });
+// Adds a line-through from the class 'line' on the list item when the checkbox is selected  
     $('.cat').on('click', '#check', function() {
-      $(this).addClass('line');
+       $(this).parent('li').addClass('line');
     });
- });
+// Removes the line-through from the class 'line' when the checkbox is deselected
+    $('.cat').on('click', '.line', function () {
+       $(this).removeClass('line');
+    });
+});
